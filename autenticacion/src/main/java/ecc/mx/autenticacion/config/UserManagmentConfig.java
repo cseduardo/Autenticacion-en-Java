e049a5ab2b3a8a3be4f893494957ab3925e8545f
Eduardo Campos S <eduardo_c.s@outlook.com>
2025-10-18 16:32:28 -0600

    implementar autorización

--- autenticacion/src/main/java/ecc/mx/autenticacion/config/UserManagmentConfig.java
+++ autenticacion/src/main/java/ecc/mx/autenticacion/config/UserManagmentConfig.java
@@ -5,14 +5,28 @@ import java.security.SecureRandom;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class UserManagmentConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() throws NoSuchAlgorithmException{
-        SecureRandom s=SecureRandom.getInstanceStrong();
-        return new BCryptPasswordEncoder(12,s);
+    public PasswordEncoder passwordEncoder() throws NoSuchAlgorithmException {
+        SecureRandom s = SecureRandom.getInstanceStrong();
+        return new BCryptPasswordEncoder(12, s);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(
+                c -> c.requestMatchers("/hello").hasAuthority("read")
+                        .requestMatchers("/hola").hasAuthority("lectura")
+                        .anyRequest().denyAll());
+
+        return http.build();
     }
 }
--- autenticacion/src/main/java/ecc/mx/autenticacion/controller/HelloController.java
+++ autenticacion/src/main/java/ecc/mx/autenticacion/controller/HelloController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController {
+    @GetMapping("/hola")
+    public String hola() {
+        return "Hola Mundo!";
+    }
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
